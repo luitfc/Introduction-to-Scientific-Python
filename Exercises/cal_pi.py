4051d07a6e23ab5cca14ac3bb649fd2ecd321eac
itfc <f.chen@lboro.ac.uk>
2018-08-31 14:45:03 +0100

    Tidy up.

--- Exercises/cal_pi.py
+++ Exercises/cal_pi.py
@@ -1,4 +1,3 @@
-
 from __future__ import print_function  # to make sure that the print statement works with both Python 2 and 3.
 from math import sqrt
 
@@ -10,10 +9,8 @@ from math import sqrt
 # function to calculate pi
 def cal_pi(k):
     value = 0
-
     for i in range(k):
         value += sqrt(12) * (-1/3)**i / (2*i+1)
-
     return value
 
 
