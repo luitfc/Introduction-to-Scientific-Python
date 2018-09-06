47532102898b80bae19b5c67395fa4312b5bea92
itfc <f.chen@lboro.ac.uk>
2018-09-06 10:47:50 +0100

    Format.

--- misc/my_prog.py
+++ misc/my_prog.py
@@ -2,18 +2,7 @@ from __future__ import print_function  # to make sure that the print statement w
 from datetime import date  # before we use a package, we need to import it first.
 
 
-class test:
-    AA = 101
-    def __init__(self):
-        self.__a = 100
-        self.AA = 2000
+def get_year_of_today():  # a function to get the year number of today
+    return date.today().year
 
-
-aa = test()
-print(aa.AA)
-bb = test()
-print(bb.AA)
-bb.AA = 202
-
-print(aa.AA)
-print(bb.AA)
+print("This year is %s." % get_year_of_today())  # call function get_year_of_today and print the result.
